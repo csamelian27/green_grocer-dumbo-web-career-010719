b7f217f0aff204a39dfe5584a3792f84c737a33d
github username <github email address>
2019-01-08 20:51:17 -0500

    work on apply_coupons method - incomplete

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,38 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  new_cart = {}
+  cart.each do |hash|
+    hash.each do |item, info|
+      new_cart[item] ||= info
+      new_cart[item][:count] ||= 0
+      new_cart[item][:count] += 1
+    end
+  end
+  return new_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  new_hash = {}
+  count = 0
+  cart.each do |cart_item, info_hash|
+    coupons.each do |coupon_item|
+      if cart_item == coupon_item[:item]
+        new_hash = {coupon_item[:item] + " W/COUPON" => {:price => coupon_item[:cost], :clearance => true, :count => count += 1}}
+      end
+    new_cart_count = info_hash[:count] - coupon_item[:num]
+    info_hash[:count] = new_cart_count
+    end
+  end
+  new_hash
+  merged_cart = cart.merge(new_hash)
+  return merged_cart
 end
 
 def apply_clearance(cart)
-  # code here
+
 end
 
 def checkout(cart, coupons)
-  # code here
+
 end
